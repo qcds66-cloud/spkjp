fc99740107e4fb4b3643a321d7ca99133acbc7cf
qcds66-cloud <qcds66@gmail.com>
2026-08-02 00:24:45 +0800

    Update spkjpan.py

--- spkjpan.py
+++ spkjpan.py
@@ -542,7 +542,7 @@ if st.sidebar.button("回到首頁 / 重新開始"):
 pause_sec = st.sidebar.slider("朗讀後暫停秒數", 0.5, 10.0, 2.5, 0.5)
 
 # --- 主畫面邏輯 ---
-st.title("會日語 單字學習系統 (Web 版)")
+st.title("會日語 單字學習(測試版) 設計:蔡鴻謀")
 
 active_list = [w for w in word_db if str(w[0]) == current_lesson_num]
 
@@ -563,7 +563,7 @@ else:
     st.markdown(
         f"""
         <div style="background-color: {COLOR['card']}; padding: 30px; border-radius: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1); margin-bottom: 20px;">
-            <h1 style="color: {COLOR['kanji']}; font-size: 30px; margin-bottom: 5px;">{data[2] or data[1]}</h1>
+            <h1 style="color: {COLOR['kanji']}; font-size: 20px; margin-bottom: 5px;">{data[2] or data[1]}</h1>
             <h2 style="color: {COLOR['kana']}; font-size: 25px; margin-top: 0px;">{data[1]}</h2>
             <h3 style="color: {COLOR['mean']}; font-size: 20px;">【 {data[3]} 】</h3>
             <hr style="border: 0; border-top: 1px solid #eee; margin: 15px 0;">
